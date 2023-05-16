8dd0343021e8817aafc96aa87c162854109a9803
Quy Nguyen <quynp@hitap.vn>
2023-05-16 20:41:10 +0700

    update

--- apk_install.py
+++ apk_install.py
@@ -10,6 +10,7 @@ class Adb:
     def __init__(self):
         self.adb = os.getcwd()+'\platform-tools\\adb.exe'
         self.apk_files = glob.glob(f'{os.getcwd()}\\apk\\*.apk')
+        self.app_package_id = "com.facebook.katana"
         subprocess.call(f"{self.adb} start-server", shell=True)
         self.client = AdbClient(host="127.0.0.1", port=5037)
 
@@ -18,6 +19,8 @@ class Adb:
         if (len(devices) > 0):
             i = 0
             for device in devices:
+                if device.is_installed(self.app_package_id) == True:
+                    subprocess.run(f"{self.adb} -s {device.serial} uninstall {self.app_package_id}", shell = True)
                 subprocess.run(f"{self.adb} -s {device.serial} install -r {self.apk_files[i]}", shell = True)
                 i += 1
         else:
