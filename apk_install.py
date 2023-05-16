8c2c812bb1973b3b63e916cd4b6396c3e2a79f10
Quy Nguyen <quynp@hitap.vn>
2023-05-16 20:02:04 +0700

    Initial commit

---
+++ apk_install.py
@@ -0,0 +1,27 @@
+
+from adb_shell.auth.keygen import keygen
+from ppadb.client import Client as AdbClient
+import subprocess
+import os, glob
+
+
+class Adb:
+
+    def __init__(self):
+        self.adb = os.getcwd()+'\platform-tools\\adb.exe'
+        self.apk_files = glob.glob(f'{os.getcwd()}\\apk\\*.apk')
+        subprocess.call(f"{self.adb} start-server", shell=True)
+        self.client = AdbClient(host="127.0.0.1", port=5037)
+
+    def installAllApp(self):
+        devices = self.client.devices()
+        if (len(devices) > 0):
+            i = 0
+            for device in devices:
+                subprocess.run(f"{self.adb} -s {device.serial} install -r {self.apk_files[i]}", shell = True)
+                i += 1
+        else:
+            print("Không có device nào")
+            return 0
+
+adb = Adb().installAllApp()
